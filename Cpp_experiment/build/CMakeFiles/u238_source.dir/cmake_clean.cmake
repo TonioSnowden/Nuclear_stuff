7b3adaa18ca8898d546bdfb7b202d0959a069042
TonioSnowden <antoine.munier@epfl.ch>
2025-01-03 09:21:33 -0800

    CPP experiment

---
+++ Cpp_experiment/build/CMakeFiles/u238_source.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/u238_source.dir/u238_source.cpp.o"
+  "CMakeFiles/u238_source.dir/u238_source.cpp.o.d"
+  "libu238_source.pdb"
+  "libu238_source.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/u238_source.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
