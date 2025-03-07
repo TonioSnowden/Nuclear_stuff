15872ccf2791d457057b6587f789288c86bb137c
TonioSnowden <antoine.munier75@gmail.com>
2025-03-06 16:31:35 -0800

    add air source

---
+++ Cpp_experiment/Pu240_source/pu240sourceair.py
@@ -0,0 +1,50 @@
+import openmc
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+pu240 = openmc.Material(name='Pu-240')
+pu240.add_nuclide('Pu240', 1.0)
+pu240.set_density('g/cm3', 19.1)  
+
+air = openmc.Material(name='Air')
+air.add_element('N', 0.784)
+air.add_element('O', 0.216)
+air.set_density('g/cm3', 0.001225)
+
+# Create a box filled with HDPE
+x_min, x_max = -10, 10
+y_min, y_max = -10, 10
+z_min, z_max = -10, 10
+
+left = openmc.XPlane(x_min, boundary_type='vacuum')
+right = openmc.XPlane(x_max, boundary_type='vacuum')
+bottom = openmc.YPlane(y_min, boundary_type='vacuum')
+top = openmc.YPlane(y_max, boundary_type='vacuum')
+front = openmc.ZPlane(z_min, boundary_type='vacuum')
+back = openmc.ZPlane(z_max, boundary_type='vacuum')
+
+sphere = openmc.Sphere(r=1.0)
+box_region = +left & -right & +bottom & -top & +front & -back
+
+# Define the HDPE cell to be the box MINUS the sphere
+air_cell = openmc.Cell(fill=air, region=box_region & ~(-sphere))
+
+# Define the Pu-240 cell to be inside the sphere
+pu240_cell = openmc.Cell(fill=pu240, region=-sphere)
+
+universe = openmc.Universe(cells=[air_cell, pu240_cell])
+geometry = openmc.Geometry(universe)
+
+source_lib = "/global/scratch/users/toniooppi/Nuclear_stuff/Cpp_experiment/Pu240_source/build/libpu240_source.so"
+
+# Settings
+settings = openmc.Settings()
+settings.run_mode = 'fixed source'
+settings.batches = 100
+settings.particles = 10  # Adjust based on your needs
+settings.source = openmc.CompiledSource(source_lib)
+
+print("Running simulation...")
+model = openmc.model.Model(geometry=geometry, settings=settings)
+sp_filename = model.run()
