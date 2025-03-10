f4e3df83f1ae2ca574e3d84160d92aac9f2371e6
TonioSnowden <antoine.munier75@gmail.com>
2025-03-10 14:40:10 -0700

    new csv handling

--- Cpp_experiment/Pu240_source/plot_track.py
+++ Cpp_experiment/Pu240_source/plot_track.py
@@ -3,38 +3,51 @@ import openmc
 import numpy as np
 import sys
 
-def get_particle_times() -> np.ndarray:
-    # Load the tracks file
-    tracks = openmc.Tracks('tracks.h5')
-
-    particle_times: list[Any] = []
-
-    # Print track data
-    for track in tracks:
-        neutron_track = track.filter(particle='neutron')
-        states = neutron_track.particle_tracks
-        for state in states:
-            particle_states = state.states  # time in seconds
-            time = particle_states["time"]
-            particle_times = np.append(particle_times, time)
-
-    return np.array(particle_times)
-
-def save_results(source_file, density, coolant_density, radius, particle_times):
-    # Filter out zero values from particle_times
-    non_zero_times = particle_times[particle_times != 0]
-    
-    with open('particle_times_output.csv', 'a') as f:
-        f.write(f"{source_file},{density},{coolant_density},{radius},{non_zero_times}\n")
-
-if __name__ == "__main__":
-    # Get parameters from command line arguments
+def main():
     source_file = sys.argv[1]
     fuel_density = sys.argv[2]
     coolant_density = sys.argv[3]
     radius = sys.argv[4]
 
-    particle_times = get_particle_times()
+    tracks = openmc.Tracks('tracks.h5')
 
-    # Save the results
-    save_results(source_file, fuel_density, coolant_density, radius, particle_times)
+    particle_times = []
+
+    for track in tracks:
+    neutron_track = track.filter(particle='neutron')
+    states = neutron_track.particle_tracks
+    for state in states:
+        particle_states = state.states  # time in seconds
+        time = particle_states["time"]
+        particle_times = np.append(particle_times, time)
+
+    particle_times = np.round(np.array(particle_times), decimals=4)
+
+    particle_times_str = np.array2string(
+        particle_times,
+        separator=',',
+        threshold=np.inf,
+        precision=4,
+        suppress_small=True
+    ).replace('\n', '')
+
+    new_data = pd.DataFrame({
+        'source_file': [source_file],
+        'fuel_density': [round(fuel_density, 4)],
+        'coolant_density': [round(coolant_density, 4)],
+        'radius': [round(radius, 4)],
+        'particle_times': [particle_times_str]
+    })
+    
+    # If file exists, append to it; if not, create new file
+    try:
+        df = pd.read_csv('particle_times_output.csv')
+        df = pd.concat([df, new_data], ignore_index=True)
+    except FileNotFoundError:
+        df = new_data
+    
+    # Write to CSV without index
+    df.to_csv('particle_times_output.csv', index=False)     
+
+if __name__ == '__main__':
+    main()
