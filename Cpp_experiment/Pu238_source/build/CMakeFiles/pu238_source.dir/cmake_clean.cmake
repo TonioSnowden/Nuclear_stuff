a311200621deff490552b57afbe14d66f79e043e
TonioSnowden <antoine.munier@epfl.ch>
2025-03-10 18:17:16 -0700

    pu238

---
+++ Cpp_experiment/Pu238_source/build/CMakeFiles/pu238_source.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pu238_source.dir/pu238_source.cpp.o"
+  "CMakeFiles/pu238_source.dir/pu238_source.cpp.o.d"
+  "libpu238_source.pdb"
+  "libpu238_source.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pu238_source.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Cpp_experiment/Pu238_source/build/CMakeFiles/pu238_source.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for pu238_source.
+# This may be replaced when dependencies are built.
